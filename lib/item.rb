08952f09fb647bbec1978adb08cfaca88782cb15
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 10:22:15 -0700

    Feat: Item prevents additional bids during close bidding

--- lib/item.rb
+++ lib/item.rb
@@ -1,17 +1,22 @@
 class Item
-  attr_reader :name, :bids
+  attr_reader :name, :bids, :close_bidding
 
-  def initialize(name, bids = {})
+  def initialize(name, bids = {}, close_bidding = {})
     @name = name
     @bids = bids
+    @close_bidding = close_bidding
   end
 
   def add_bid(bidder, amount)
-    @bids[bidder] = amount
+    @bids[bidder] = amount if @close_bidding[:on?] != true
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @close_bidding[:on?] = true
   end
 
 end
