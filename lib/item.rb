004a0fdfeafde791ad2978baa81e66ffd72840bb
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:52:00 -0500

    Feat: add bid method

--- lib/item.rb
+++ lib/item.rb
@@ -6,4 +6,12 @@ class Item
     @bids = Hash.new(0)
   end
 
+  def add_bid(attendee, bid)
+    if !@bids.has_key?(attendee)
+      @bids[attendee] = bid
+      # require 'pry'; binding.pry
+    end
+    @bids[attendee]
+  end
+
 end
