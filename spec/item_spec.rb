8cc42ca6abe54eb1c4fb1bec52af04f5a57c050e
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-08 12:25:28 -0500

    Create #close_bidding test.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -35,4 +35,32 @@ RSpec.describe 'Item' do
     end
   end
 
+  describe 'Iteration 3' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookie')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    it 'Closes bidding on individual items' do
+      expect(item1.bids).to eq({ attendee1 => 22, attendee2 => 20 })
+      item1.close_bidding
+      item1.add_bid(attendee3, 70)
+      expect(item1.bids).to eq({ attendee1 => 22, attendee2 => 20 })
+    end
+  end
+
 end
