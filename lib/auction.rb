b321d5225f10884072f92fd3cf07361b467b6cc2
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:21:55 -0700

    Feat: Auction exists

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,18 +1,8 @@
-require 'rspec'
+require 'pry'
 require './lib/item'
 require './lib/attendee'
-require './lib/auction'
 
-RSpec.describe Auction do
-  before (:each) do
-    @auction = Auction.new
-    @item1 = Item.new('Chalkware Piggy Bank')
-    @item2 = Item.new('Bamboo Picture Frame')
-    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
-  end
+class Auction
 
-  it 'exists' do
-    expect(@auction).to be_an_instance_of(Auction)
-  end
-
+  
 end
