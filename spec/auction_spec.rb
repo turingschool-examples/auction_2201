f487753fb36d04ee12057975b89fe605ec06a090
Sherman Au <shermau13@gmail.com>
2022-03-08 09:56:33 -0700

    Auction #add_item

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require './lib/items'
 require './lib/auction'
 
 RSpec.describe Auction do
@@ -13,6 +14,20 @@ RSpec.describe Auction do
 
     it 'Auction #item is an empty array' do
       expect(@auction.items).to eq([])
+    end
+  end
+  context 'Auction adding items' do
+    before(:each) do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    it 'Auction #add_item' do
+      expect(@auction.items).to eq([@item1, @item2])
+    end
+
+    it 'Auction #item is populates' do
     end
   end
 end
