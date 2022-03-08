027419ebd0ef978baac2e4d3923ce10640d400fe
Drew Proebstel <d.proebstel@gmail.com>
2022-03-08 08:50:42 -0800

    current high bid

--- lib/item.rb
+++ lib/item.rb
@@ -8,4 +8,14 @@ class Item
   def add_bid(attendee,bid)
     bids[attendee] = bid
   end
+
+  def current_high_bid
+    high_bid = 0
+    bids.each do |bid|
+      if bid[1] > high_bid
+        high_bid = bid[1]
+      end
+    end
+    return high_bid
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe 'auction' do
     expect(item1.bids).to eq({})
   end
 
-  it 'items start with empty bid hash' do
+  it 'itemscan take bids' do
     item1 = Item.new('Chalkware Piggy Bank')
     item2 = Item.new('Bamboo Picture Frame')
     item3 = Item.new('Homemade Chocolate Chip Cookies')
@@ -83,6 +83,26 @@ RSpec.describe 'auction' do
     item1.add_bid(attendee2, 20)
     item1.add_bid(attendee1, 22)
     expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+  end
+
+  it 'high bid' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    expect(item1.current_high_bid).to eq(22)
   end
 
 
