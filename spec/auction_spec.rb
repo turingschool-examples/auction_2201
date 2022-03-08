14d25cc51a9482a03ee4b59165056f30f641048f
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 09:41:36 -0700

    test: Auction #unpopular_items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,6 +3,14 @@ require './lib/auction'
 
 describe Auction do
   before :each do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     @auction = Auction.new
   end
 
@@ -18,21 +26,37 @@ describe Auction do
 
   context 'adding items' do
     before :each do
-      @item1 = Item.new('Chalkware Piggy Bank')
-      @item2 = Item.new('Bamboo Picture Frame')
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
     end
 
     it 'Auction #add_item' do
-      @auction.add_item(@item1)
-      @auction.add_item(@item2)
-
       expect(@auction.items).to eq([@item1, @item2])
     end
 
     it 'Auction #item_names' do
+      expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    end
+  end
+
+  context 'queries on item bids' do
+    before :each do
       @auction.add_item(@item1)
       @auction.add_item(@item2)
-      expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+    end
+
+    it 'auction #unpopular_items' do
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
     end
   end
 end
