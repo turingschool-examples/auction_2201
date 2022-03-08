05cadbc9fdb6e237836f6937410145ca80937aed
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:24:05 -0500

    Test: write test for add item

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Auction do
     auction = Auction.new
     expect(auction.items).to eq([])
   end
+
+  it "can add items to items array" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.items).to eq([item1, item2])
+  end 
 end
