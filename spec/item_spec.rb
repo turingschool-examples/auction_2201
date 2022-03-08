58c81cb69e9c6749aa5dada5704eb97b27366451
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-08 09:15:30 -0700

    Test: Attendee exists

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,10 @@
 require "./lib/item"
 
 RSpec.describe Item do
+  item1 = Item.new("Chalkware Piggy Bank")
+  item2 = Item.new("Bamboo Picture Frame")
+
   it "exists/ has readable attributes" do
-    item1 = Item.new("Chalkware Piggy Bank")
     expect(item1).to be_an_instance_of(Item)
     expect(item1.name).to eq("Chalkware Piggy Bank")
     expect(item1.bids).to eq({})
