41ac484d55a4b26c52f7b5d83f3c01ae194e80bd
Luke Pascale <lpluke5@gmail.com>
2022-03-08 11:26:30 -0700

    feat: Add ability to stop bidding and updated add_bid method

--- lib/item.rb
+++ lib/item.rb
@@ -1,18 +1,25 @@
 class Item
-  attr_reader :name, :bids, :bidders
+  attr_reader :name, :bids, :bidders, :open_to_bids
 
   def initialize(name)
     @name = name
     @bids = {}
     @bidders = []
+    @open_to_bids = true
   end
 
   def add_bid(attendee, quantity)
-    @bids[attendee] = quantity
-    @bidders << attendee.name
+    if @open_to_bids == true
+      @bids[attendee] = quantity
+      @bidders << attendee.name
+    end
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @open_to_bids = false
   end
 end
