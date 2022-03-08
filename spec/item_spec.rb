746d0db89e2736d7416a1480335764efd23ba259
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-08 11:07:31 -0500

    Create readable attributes test for attendee.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,12 +1,16 @@
 require './lib/item'
 
 RSpec.describe 'Item' do
-  describe 'iteration 1' do
+  describe 'Iteration 1' do
     item1 = Item.new('Chalkware Piggy Bank')
 
     it 'Has a readable name attribute' do
       expect(item1.name).to eq 'Chalkware Piggy Bank'
     end
+
+    item2 = Item.new('Bamboo Picture Frame')
+
+    
   end
 
 end
