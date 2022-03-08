33e705d8f18e799045b6a3ea2304f49ac1d8db63
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-08 09:45:29 -0700

    Auction can determine it's potential revenue

--- lib/auction.rb
+++ lib/auction.rb
@@ -16,4 +16,11 @@ class Auction
   def unpopular_items
     @items.find_all{|item| item.bids.length < 1 }
   end
+
+  def potential_revenue
+    high_bids = []
+    @items.each {|item| high_bids << item.current_high_bid}
+    high_bids.reject! {|bid| bid.nil?}
+    high_bids.sum
+  end
 end
