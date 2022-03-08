fd1011ff1434f22472f6bc42c3e62574aa9fa4a7
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-08 11:37:12 -0500

    #bids attribute test made.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,10 +7,18 @@ RSpec.describe 'Item' do
     it 'Has a readable name attribute' do
       expect(item1.name).to eq 'Chalkware Piggy Bank'
     end
+  end
 
+  describe 'Iteration 2' do
+    item1 = Item.new('Chalkware Piggy Bank')
     item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookie')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
 
-    
+    it 'Has a bids attribute' do
+      expect(item1.bids).to eq {}
+    end
   end
 
 end
