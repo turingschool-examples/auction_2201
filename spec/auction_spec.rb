a934a9e763db47da8b493470a263de463bb87b40
Chris Romero <chrisleeromero1@gmail.com>
2022-03-08 10:20:04 -0700

    Test: add test for bidders method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -84,12 +84,11 @@ RSpec.describe Auction do
     @auction.add_item(@item3)
     @auction.add_item(@item4)
     @auction.add_item(@item5)
-    @item1.add_bid(@attendee2, 20)
     @item1.add_bid(@attendee1, 22)
-    expect(@item1.current_high_bid).to eq(22)
+    @item1.add_bid(@attendee2, 20)
     @item4.add_bid(@attendee3, 50)
     @item3.add_bid(@attendee2, 15)
-    expect(@auction.potential_revenue).to eq(87)
+    expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
   end
 
 end
