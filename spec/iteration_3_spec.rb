8646d595b4f880ad282d830cc227097b3187c330
John Hennerich <john.hennerich@gmail.com>
2022-03-08 11:20:31 -0700

    Test: bidders to spec test

---
+++ spec/iteration_3_spec.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require 'rspec'
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Auction do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    @auction = Auction.new
+  end
+
+  it '#bidders' do
+
+
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    @item1.add_bid(@attendee1, 22)
+    @item1.add_bid(@attendee2, 20)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+
+    expected = ["megan", "Bob", "Mike"]
+    expect(@auction.bidders).to eq(expected)
+  end
+end
