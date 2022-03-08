85e9bd033935e8790b382b367f3c22539463dd24
Susan Boyd <sueboyd922@gmail.com>
2022-03-08 11:37:32 -0500

    Test: test for potential revenue of auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -63,8 +63,18 @@ describe Auction do
     it 'can tell you the unpopular items' do
       @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
-      @item3.add_bid(@attendee3, 50)
-      expect(@auction.unpopular_items).to eq([@item2, @item4, @item5])
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
+    end
+
+    it 'has potential revenue' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.potential_revenue).to eq(87)
     end
   end
 
