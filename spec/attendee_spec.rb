6f7016dbc1c13bd6e5b047cc86fdfe3ba80e9fc8
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 09:16:02 -0700

    Ref: budget as unique method not attribute reader

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe Attendee do
     @attendee = Attendee.new({name: 'Megan', budget: '$50'})
   end
 
-  it 'initializes with a readable name and budget' do
+  it 'initializes with a readable name' do
     expect(@attendee.name).to eq("Megan")
+  end
+
+  it 'returns given string budget as an integer' do
     expect(@attendee.budget).to eq(50)
   end
 
