d8b4792a84630e009f14527ee51de6d043a9a339
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 09:30:00 -0700

    Test: Item #add_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,6 +5,13 @@ describe Item do
   before :each do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    @auction = Auction.new
   end
 
   context 'initialization' do
@@ -21,6 +28,23 @@ describe Item do
     it 'starts with no bids' do
       expect(@item1.bids).to eq({})
       expect(@item2.bids).to eq({})
+    end
+  end
+
+  context 'adding bids' do
+    before :each do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+    end
+
+    it 'Item #add_bid' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee3 => 22})
     end
   end
 end
