ecaa520ce41ae3ab434679108b406fc652c8afac
Will Medders <wmedders@gmail.com>
2022-03-08 11:28:29 -0500

    Spec: auction #item_names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -22,4 +22,12 @@ RSpec.describe Auction do
     @auction.add_item(item2)
     expect(@auction.items).to eq([item1, item2])
   end
+
+  it '#item_names' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    @auction.add_item(item1)
+    @auction.add_item(item2)
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
 end
