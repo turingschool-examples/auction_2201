5e9034718afdc79a9371953eee93f38888630f64
John Hennerich <john.hennerich@gmail.com>
2022-03-08 10:57:52 -0700

    Feat: implement unpopular_items

--- lib/auction.rb
+++ lib/auction.rb
@@ -14,4 +14,18 @@ class Auction
 #    item_names_array = @items.map { |name| name.name }
 #    item_names_array
   end
+
+  def unpopular_items
+    items.find_all do |item|
+      item.bids == {}
+    end
+#    unpopular = []
+#    @items.each do |item|
+#      binding.pry
+#      if @item.bids is nil
+#        unpopular << item
+#      end
+#    end
+#    unpopular
+  end
 end
--- spec/iteration_2_spec.rb
+++ spec/iteration_2_spec.rb
@@ -40,5 +40,19 @@ RSpec.describe Auction do
     expect(@item1.current_high_bid).to eq(22)
   end
 
-  
+  it '#unpopular_items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+
+    expected = [@item2, @item3, @item5]
+    expect(@auction.unpopular_items).to eq(expected)
+
+  end
 end
