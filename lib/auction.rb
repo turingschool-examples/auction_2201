41310a369c2c572d049472ce5a4435b967c8312a
John Hennerich <john.hennerich@gmail.com>
2022-03-08 09:37:47 -0700

    Feat: create add_item, item_names in auction iteration 1 complete

--- lib/auction.rb
+++ lib/auction.rb
@@ -4,4 +4,14 @@ class Auction
   def initialize()
     @items = []
   end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def item_names
+    @items.map { |name| name.name }
+#    item_names_array = @items.map { |name| name.name }
+#    item_names_array
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,6 +1,8 @@
 require 'pry'
 require 'rspec'
+require './lib/item'
 require './lib/auction'
+require './lib/attendee'
 
 RSpec.describe Auction do
   before(:each) do
