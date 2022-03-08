79531b36d71d09e461067f183e78258dd4955c5c
Rin Fabian <pfabianschool@gmail.com>
2022-03-08 10:19:15 -0600

    test; #item_names for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -16,7 +16,12 @@ RSpec.describe Auction do
     @auction.add_item(@item1)
     @auction.add_item(@item2)
     expect(@auction.items).to eq([@item1, @item2])
+  end
 
+  it 'can see item names' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank","Bamboo Picture Frame"])
   end
 
 end
