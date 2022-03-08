55d35588075c0efc53eed0fe42b744d5c5993c3a
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 11:29:12 -0700

    Fix: Change order of bids placed to match interaction patterin in Iteration 3

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -91,12 +91,13 @@ RSpec.describe Auction do
       @auction.add_item(@item4)
       @auction.add_item(@item5)
 
-      @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
       @item4.add_bid(@attendee3, 50)
       @item3.add_bid(@attendee2, 15)
     end
-
+    # I'll be honest I don't see why this expects Megan's name first!
+    # OH MY GOD the interaction pattern for iteration 3 has her bid come in first.
     it "returns all bidders' names through #bidders method" do
       expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
     end
@@ -117,7 +118,7 @@ RSpec.describe Auction do
     end
 
     it "has helper method attendees to return all attendee objects who have bid on an item" do
-      expect(@auction.attendees).to eq([@attendee2, @attendee1, @attendee3])
+      expect(@auction.attendees).to eq([@attendee1, @attendee2, @attendee3])
     end
 
   end
