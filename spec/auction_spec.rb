3c36dbc16c1255b065e1f68a6dbf0abe796509e1
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-08 08:42:51 -0800

    Test: fix auction_spec test to not include item test, item test now has correct test and bids initialized

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -10,7 +10,6 @@ require './lib/attendee'
       expect(auction.items).to eq([])
     end
 
-
     it 'exists and has an empty array' do
       auction = Auction.new
       item1 = Item.new('Chalkware Piggy Bank')
@@ -24,21 +23,4 @@ require './lib/attendee'
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
 
-    it 'item 1 bid is an empty hash currently' do
-       item1 = Item.new('Chalkware Piggy Bank')
-       item2 = Item.new('Bamboo Picture Frame')
-       item3 = Item.new('Homemade Chocolate Chip Cookies')
-       item4 = Item.new('2 Days Dogsitting')
-       item5 = Item.new('Forever Stamps')
-       attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
-       attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
-       attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
-       auction = Auction.new
-        auction.add_item(item1)
-        auction.add_item(item2)
-        auction.add_item(item3)
-        auction.add_item(item4)
-        auction.add_item(item5)
-        expect(item1.bids).to be_an(Hash)
-        expect(item1.bids).to be({})
-  end
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,4 +9,24 @@ require './lib/attendee'
       expect(item1).to be_an_instance_of(Item)
       expect(item1.name).to eq("Chalkware Piggy Bank")
     end
+
+    it 'item 1 bid is an empty hash currently' do
+       item1 = Item.new('Chalkware Piggy Bank')
+       item2 = Item.new('Bamboo Picture Frame')
+       item3 = Item.new('Homemade Chocolate Chip Cookies')
+       item4 = Item.new('2 Days Dogsitting')
+       item5 = Item.new('Forever Stamps')
+       attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+       attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+       attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+       auction = Auction.new
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+      expect(item1.bids).to be_an(Hash)
+      expect(item1.bids).to eq({})
+
+  end
 end
