9d959ee0d5d0dc14a4fc3afe4e304502427a2727
Chris Romero <chrisleeromero1@gmail.com>
2022-03-08 09:14:27 -0700

    Test: add test for attendee attributes

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,6 +1,6 @@
-require './lib/item'
+require './lib/attendee'
 
-RSpec.describe Item do
+RSpec.describe Attendee do
 
   before(:each) do
     @attendee = Attendee.new({name: 'Megan', budget: '$50'})
@@ -8,6 +8,11 @@ RSpec.describe Item do
 
   it 'exists' do
     expect(@attendee).to be_a(Attendee)
+  end
+
+  it 'has attributes' do
+    expect(@attendee.name).to eq("Megan")
+    expect(@attendee.budget).to eq(50)
   end
 
 end
