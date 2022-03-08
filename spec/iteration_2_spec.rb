33316b28df81687fc07914de438af49d9f1bcee5
John Hennerich <john.hennerich@gmail.com>
2022-03-08 09:54:33 -0700

    Test: create iteration_2_spec to use

---
+++ spec/iteration_2_spec.rb
@@ -0,0 +1,22 @@
+require 'pry'
+require 'rspec'
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Iteraion2 do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    @auction = Auction.new
+  end
+
+
+
+end
