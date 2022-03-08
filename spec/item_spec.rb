50e0eb119571ef0cd494bfb3612789c60e8a98cd
Rin Fabian <pfabianschool@gmail.com>
2022-03-08 10:28:06 -0600

    test; #bid for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,13 +1,19 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do
   before (:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
   end
 
   it 'has a name' do
     expect(@item1.name).to eq('Chalkware Piggy Bank')
     expect(@item2.name).to eq('Bamboo Picture Frame')
+  end
+
+  it 'starts with no bids' do
+    expect(@item1.bids).to eq({})
   end
 end
