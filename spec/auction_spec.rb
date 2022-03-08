cc45762edded4568a96c58cef0a14d312561d3c0
Sherman Au <shermau13@gmail.com>
2022-03-08 09:51:42 -0700

    test:Auction #item is an empty array

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,7 +2,7 @@ require 'pry'
 require './lib/auction'
 
 RSpec.describe Auction do
-  context "Iteration 1" do
+  context 'Iteration 1' do
     before(:each) do
       @auction = Auction.new
     end
@@ -11,4 +11,8 @@ RSpec.describe Auction do
       expect(@uaction).to be_a(Auction)
     end
 
+    it 'Auction #item is an empty array' do
+      expect(@auction.items).to eq([])
+    end
   end
+end
