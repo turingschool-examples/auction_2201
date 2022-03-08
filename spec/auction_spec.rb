c8f9d0902e23cffde34e7d0ef8052ab0edfd785b
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:45:59 -0700

    Test: Auctio identifies unpopular items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe Auction do
     @auction = Auction.new
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
-    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   it 'exists' do
@@ -22,13 +27,28 @@ RSpec.describe Auction do
   it 'adds items to items array' do
     @auction.add_item(@item1)
     @auction.add_item(@item2)
+
     expect(@auction.items).to eq([@item1, @item2])
   end
 
   it 'calls an array of item names' do
     @auction.add_item(@item1)
     @auction.add_item(@item2)
+
     expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
+
+  it 'identifies unpopular items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+
+    expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
   end
 
 end
