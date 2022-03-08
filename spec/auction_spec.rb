77720b5b5f765d6059e8d7da4edffa5c4a6062c7
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:39:03 -0500

    Test: write pattern for Iteration 2, write test for item bids

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -31,4 +31,28 @@ RSpec.describe Auction do
     auction.add_item(item2)
     expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
+
+  context "Iteration 2" do
+    before :each do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      @auction = Auction.new
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+    end
+
+    it "item starts with empty hash of bids" do
+      expected = ({})
+      expect(@item1.bids).to eq(expected)
+    end
+  end
 end
