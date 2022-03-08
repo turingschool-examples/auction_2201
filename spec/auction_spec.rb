0c56407c648874d07925e95eb66fe1e3d6e2b2b4
Drew Proebstel <d.proebstel@gmail.com>
2022-03-08 08:34:32 -0800

    test for bids

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -47,6 +47,24 @@ RSpec.describe 'auction' do
     expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
 
+  it 'action adds items' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    expect(item1.bids).to eq({})
+  end
+
 
 
 
