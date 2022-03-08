a721362124da5a0e370cde562e4a1c14d40e7de6
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-08 09:30:50 -0800

    Test: add attendee 1 current highest bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -26,7 +26,7 @@ require './lib/attendee'
       auction.add_item(item4)
       auction.add_item(item5)
       expect(item1.bids).to eq({})
-    end
+     end
 
     it 'item 1 bid has two bids' do
        item1 = Item.new('Chalkware Piggy Bank')
@@ -47,4 +47,25 @@ require './lib/attendee'
       item1.add_bid(attendee1, 22)
       expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
   end
+
+  it 'attendee 1 has current highest bid' do
+     item1 = Item.new('Chalkware Piggy Bank')
+     item2 = Item.new('Bamboo Picture Frame')
+     item3 = Item.new('Homemade Chocolate Chip Cookies')
+     item4 = Item.new('2 Days Dogsitting')
+     item5 = Item.new('Forever Stamps')
+     attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+     attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+     attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+     auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    expect(item1.current_height_bid).to eq(22)
+  end
+
 end
