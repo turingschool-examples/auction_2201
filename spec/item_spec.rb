ab52a9e6cf7dead70c3020b28efcf90e9353f8bd
Sherman Au <shermau13@gmail.com>
2022-03-08 09:07:32 -0700

    q

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,15 +2,16 @@ require 'pry'
 require './lib/item'
 
 RSpec.describe Item do
-  context "Iteration 1" do
+  context 'Iteration 1' do
     before(:each) do
-
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
     end
 
-    xit 'exists ' do
-      expect().to be_an_instance_of()
+    it 'exists ' do
+      expect(@item1).to be_an_instance_of(Item)
+      expect(@item2).to be_an_instance_of(Item)
     end
-
   end
   # context "Iteration 2" do
   #   before(:each) do
@@ -29,5 +30,4 @@ RSpec.describe Item do
   #     expect().to eq()
   #   end
   # end
-
 end
