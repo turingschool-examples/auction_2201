bf069630b5c8768ded812c800d4e691e1bb254fc
Chris Romero <chrisleeromero1@gmail.com>
2022-03-08 09:27:02 -0700

    Feat: add item_names method to complete iteration 1

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,9 +1,13 @@
 require './lib/auction'
+require './lib/item'
+
 
 RSpec.describe Auction do
 
   before(:each) do
     @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
 
@@ -13,11 +17,16 @@ RSpec.describe Auction do
 
   it 'has items' do
     expect(@auction.items).to eq([])
-    @auction.add_item(item1)
-    @auction.add_item(item2)
-    expect(@auction.items).to eq([item1,item2])
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.items).to eq([@item1,@item2])
   end
 
-
+  it 'has names for the items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    require 'pry'; binding.pry
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
 
 end
