7cd8ccadec1c71e279ca23b8973f45bfc7746161
reuben <creubend@gmail.com>
2022-03-08 10:00:43 -0700

    test: Item unpopular items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,6 +54,17 @@ RSpec.describe Attendee do
       @item1.add_bid(@attendee1, 22)
       expect(@item1.bids).to eq ({@attendee2 => 20, @attendee1 => 22})
     end
+    it "returns high bid" do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+    end
+    it "returns high bid" do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@item1.unpopular_items).to eq([@item2, @item3, @item5])
+    end
   end
 
 
