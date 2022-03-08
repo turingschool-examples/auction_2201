9b8df51467bf92a66a2a019bee6272843c7ba32c
Rin Fabian <pfabianschool@gmail.com>
2022-03-08 10:48:42 -0600

    test;#potential_revenue for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -44,5 +44,19 @@ RSpec.describe Auction do
     expect(@auction.unpopular_items).to eq([@item2,@item3,@item5])
   end
 
+  it 'can calculate the total revenue' do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    expect(@auction.potential_revenue).to eq(87)
+  end
+
 
 end
