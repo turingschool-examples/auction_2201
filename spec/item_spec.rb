bff2c68e02353a93fb7394ed0a7869fd9fd481bb
John Hennerich <john.hennerich@gmail.com>
2022-03-08 09:10:20 -0700

    Feat: creat item per item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,14 +4,14 @@ require './lib/item'
 
 RSpec.describe Item do
   before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
   end
 
   it "#exists" do
-    item1 = Item.new('Chalkware Piggy Bank')
-    expect(item1).to be_an_instance_of(Item)
+    expect(@item1).to be_an_instance_of(Item)
   end
 
   it "has attributes" do
-    expect(item1.name).to eq('Chalkware Piggy Bank')
+    expect(@item1.name).to eq('Chalkware Piggy Bank')
   end
 end
