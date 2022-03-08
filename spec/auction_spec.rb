e2ccf5017d0f0e00f3851bc3ff36e6d984579bdd
Will Medders <wmedders@gmail.com>
2022-03-08 12:26:45 -0500

    Spec: Auction #close_bidding

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -124,5 +124,12 @@ RSpec.describe Auction do
     it '#bidders' do
       expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
     end
+
+    it '#close_bidding' do
+      expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})
+      item1.close_bidding
+      item1.add_bid(@attendee3, 70)
+      expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})      
+    end
   end
 end
