d926ffdd774af61524ebff75b451f04c4df21e0d
Luke Pascale <lpluke5@gmail.com>
2022-03-08 10:20:46 -0700

    test: Write test for potential_revenue

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -89,5 +89,14 @@ RSpec.describe Auction do
 
       expect(@auction.unpopular_items).to eq([@item2, @item5])
     end
+
+    it 'can return potential revenue' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.potential_revenue).to eq(87)
+    end
   end
 end
