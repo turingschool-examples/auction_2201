59432b944127bc57f92f7dfd7569bbc9be22b2f0
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 10:44:32 -0700

    Test: Item #close_bidding

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -45,4 +45,32 @@ RSpec.describe Item do
     end
 
   end
+
+  context "Iteration 3" do
+
+    before :each do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+    end
+
+    it "can close bidding" do
+      @item1.close_bidding
+      @item1.add_bid(@attendee3, 70)
+
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+    end
+
+  end
 end
