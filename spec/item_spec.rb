cab12a34ce437346c8806d8f8dd476c51e931564
Chris Romero <chrisleeromero1@gmail.com>
2022-03-08 09:06:01 -0700

    Test: add first test for initializing an item as initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+  end
+
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+  end
+
+
+
+end
