45f01d403fae7f0cd39a4fba3747d08f9e622cd5
Kim G <xiaole.guo@yahoo.com>
2022-03-08 09:05:45 -0700

    Item exists and has a name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,5 +3,14 @@ require './lib/item'
 describe Item do
 
 
+  before (:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+  end
+  it 'exists' do
+    expect(@item1).to be_a Item
+  end
 
+  it 'has a name' do
+    expect(@item1.name).to eq 'Chalkware Piggy Bank'
+  end
 end
