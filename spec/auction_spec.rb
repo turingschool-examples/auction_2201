619b8045044e24439717cc6ce9efb8cd7cda30d9
James Harkins <jh.harkins@protonmail.com>
2022-03-08 09:35:36 -0700

    test: create tests for auction class unpopular_items and potential_revenue methods

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,4 +1,4 @@
-require './lib/auction'
+ require './lib/auction'
 require 'pry'
 
 RSpec.describe Auction do
@@ -7,6 +7,12 @@ RSpec.describe Auction do
     @auction = Auction.new
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   context 'Iteration 1' do
@@ -26,4 +32,33 @@ RSpec.describe Auction do
       expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
     end
   end
+
+  context 'Iteration 2' do
+    it 'can find all items without any bids' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq(@item2, @item3, @item5)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq(@item3, @item5)
+    end
+
+    it 'can determine potential revenue based on current high bids for each item' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.potential_revenue).to eq(87)
+    end
+  end 
 end
