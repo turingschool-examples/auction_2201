352919cf7144bdfc036eeb287de14581c1bde619
“Brad <hutch13531@gmail.com>
2022-03-08 13:47:29 -0500

    created test for attendee attributes

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe do
     expect(attendee).to be_an_instance_of(Attendee)
   end
 
+  it "has attributes"do
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    expect(attendee.name).to eq("Megan")
+  end
+
+
 end
