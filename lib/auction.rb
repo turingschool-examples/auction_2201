024ad4d95fd1fcfe12f6674e326c9efa72c64e0f
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-08 08:32:05 -0800

    Feat: add add_item and item_names method with pass test

--- lib/auction.rb
+++ lib/auction.rb
@@ -5,4 +5,14 @@ class Auction
     @items = []
   end
 
+  def add_item(item)
+    @items << item
+  end
+
+  def item_names
+    @items.map do |item|
+      item.name
+    end
+  end
+
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -9,4 +9,19 @@ require './lib/attendee'
       expect(auction.items).to be_a(Array)
       expect(auction.items).to eq([])
     end
+
+
+    it 'exists and has an empty array' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      # expect(auction).to be_an_instance_of(Auction)
+      # expect(auction.items).to be_a(Array)
+      # expect(auction.items).to eq([])
+      auction.add_item(item1)
+      auction.add_item(item2)
+      expect(auction.items).to eq([item1, item2])
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+
   end
