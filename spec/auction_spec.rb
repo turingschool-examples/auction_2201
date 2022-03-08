71e84b6e321989e7fc2483bffebbd3ea90a4cdeb
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 10:22:48 -0700

    Test: Auction #bidder_info

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -39,15 +39,15 @@ describe Auction do
     end
   end
 
-  context 'queries on item bids' do
+  context 'queries on items and bids' do
     before :each do
       @auction.add_item(@item1)
       @auction.add_item(@item2)
       @auction.add_item(@item3)
       @auction.add_item(@item4)
       @auction.add_item(@item5)
-      @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
       @item4.add_bid(@attendee3, 50)
     end
 
@@ -66,7 +66,30 @@ describe Auction do
     end
 
     it 'Auction #bidders' do
-      expect(@auction.bidders).to eq(["Bob", "Megan", "Mike"])
+      expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
+    end
+
+    it 'Auction #bidder_info' do
+      @item3.add_bid(@attendee2, 15)
+      expected = {
+        @attendee1 =>
+          {
+            :budget => 50,
+            :items => [@item1]
+          },
+        @attendee2 =>
+          {
+            :budget => 75,
+            :items => [@item1, @item3]
+          },
+        @attendee3 =>
+          {
+            :budget => 100,
+            :items => [@item4]
+          }
+      }
+
+      expect(@auction.bidder_info).to eq(expected)
     end
   end
 end
