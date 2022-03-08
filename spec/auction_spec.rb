75f742ea3d0137d2d6d930984772bfe11eb1201b
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-08 11:47:29 -0500

    #add_bid test made.

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe 'Auction' do
       expect(auction.item_names).to eq [item1.name, item2.name]
     end
   end
+
+  describe 'Iteration 2' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookie')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -19,6 +19,15 @@ RSpec.describe 'Item' do
     it 'Has a bids attribute' do
       expect(item1.bids).to eq({})
     end
+
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+    it 'can add bids to an item' do
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
   end
 
 end
