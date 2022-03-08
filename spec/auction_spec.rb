4c51727beb2b885a637030bd4a2546299d2a8a62
Will Medders <wmedders@gmail.com>
2022-03-08 11:56:10 -0500

    Spec: Auction #unpopular_items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -72,6 +72,16 @@ RSpec.describe Auction do
       expect(@item1.current_high_bid).to eq(22)
     end
 
-    
+    it '#unpopular items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+    end
   end
 end
