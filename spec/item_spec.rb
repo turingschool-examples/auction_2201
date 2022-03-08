4d6181064e5643afd36026df5582bd5df4e7c434
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:37:57 -0700

    Test: Item returns current highest bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -27,7 +27,13 @@ RSpec.describe Item do
     @item1.add_bid(@attendee2, 20)
     @item1.add_bid(@attendee1, 22)
     expect(@item1.bids).to eq({@attendee2 => 20,
-      @attendee2 => 22})
+      @attendee1 => 22})
+  end
+
+  it "returns current highest bid" do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    expect(@item1.current_high_bid).to eq(22)
   end
 
 end
