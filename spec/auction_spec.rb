ffd60bb2b37d79a22372b16bd27a10e920962034
Will Medders <wmedders@gmail.com>
2022-03-08 12:51:12 -0500

    Spec: Auction #bidder_info

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -131,5 +131,16 @@ RSpec.describe Auction do
       @item1.add_bid(@attendee3, 70)
       expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})
     end
+
+    it '#bidder_info' do
+      expect(@auction.bidder_info).to eq({
+        @attendee1 => {
+          :budget => 50, :items => [@item1]},
+        @attendee2 => {
+          :budget => 75, :items => [@item1, @item3]},
+        @attendee3 => {
+          :budget => 100, :items => [@item4]}
+          })
+    end
   end
 end
