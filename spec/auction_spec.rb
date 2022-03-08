d2bef73dd809e37bbdeeacfcdf015ebabf4712b5
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-08 09:40:52 -0700

    Test: Auction exists and has no items

---
+++ spec/auction_spec.rb
@@ -0,0 +1,26 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+require 'pry'
+
+RSpec.describe Auction do
+  context "Iteration 1" do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+
+      @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+
+      @auction = Auction.new
+    end
+
+    it 'exists' do
+      expect(@auction).to be_an_instance_of(Auction)
+    end
+
+    it "has no items by default" do
+      expect(@auction.items).to eq([])
+    end
+
+  end
+end
