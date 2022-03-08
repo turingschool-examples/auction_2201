9d1c0d53f019c360dff57e5f8c3932dd1bfb2944
Will Medders <wmedders@gmail.com>
2022-03-08 11:06:22 -0500

    Spec: item exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require 'pry'
+require 'rspec'
+
+RSpec.describe Item do
+  before :each do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  it 'exists' do
+    expect(item1).to be_a(Item)
+  end
+end
