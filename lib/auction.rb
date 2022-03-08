9ea9f70c7664cb6b181c4c4ab48c0407b2d861d2
Drew Proebstel <d.proebstel@gmail.com>
2022-03-08 09:04:28 -0800

    iteration 2 done

--- lib/auction.rb
+++ lib/auction.rb
@@ -15,4 +15,10 @@ class Auction
   def unpopular_items
     items.find_all {|item| item.bids.length == 0}
   end
+
+  def potential_revenue
+    high_bids = items.map {|item| item.current_high_bid}
+    high_bids.sum
+    binding.pry
+  end
 end
