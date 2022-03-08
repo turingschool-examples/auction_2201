cca7325308113bf8ceaaf61e9d31853c5bec6183
John Hennerich <john.hennerich@gmail.com>
2022-03-08 09:14:06 -0700

    Test: create attendee_spec

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,18 @@
+require 'pry'
+require 'rspec'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  before(:each) do
+    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+  end
+
+  it "#exists" do
+    expect(@attendee).to be_an_instance_of(Attendee)
+  end
+
+  it "has attributes" do
+    expect(@attendee.name).to eq('Megan')
+    expect(@attendee.budget).to eq(50)
+  end
+end
