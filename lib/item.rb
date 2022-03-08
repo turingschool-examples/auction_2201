28cd72128117056f78e14ac25fad8f547aa267f0
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 10:13:15 -0700

    Feat: Item #close_bidding passing test

--- lib/item.rb
+++ lib/item.rb
@@ -4,13 +4,22 @@ class Item
   def initialize(name)
     @name = name
     @bids = {}
+    @open = true
   end
 
   def add_bid(attendee, bid)
-    @bids[attendee] = bid
+      @bids[attendee] = bid if open?
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @open = false
+  end
+
+  def open?
+    @open
   end
 end
