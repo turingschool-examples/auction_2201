503d910d17c260fbff58a9d15da4ca6c6e277a4a
Sherman Au <shermau13@gmail.com>
2022-03-08 09:45:54 -0700

    refactor:require lib/attendee

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,14 +1,19 @@
 require 'pry'
-require './lib/'
+require './lib/attendee'
 
-RSpec.describe  do
-  context "Iteration 1" do
+RSpec.describe do
+  context 'Iteration 1' do
     before(:each) do
-      @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee = Attendee.new({ name: 'Megan', budget: '$50' })
     end
 
     it 'exists ' do
       expect(@attendee).to be_a(Attendee)
     end
 
+    it 'has attributes' do
+      expect(@attendee.name).to eq('Megan')
+      expect(@attendee.budget).to eq('50')
+    end
   end
+end
