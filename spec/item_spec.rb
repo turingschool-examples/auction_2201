2937c0ea93f35c4c175763f23ad2b2a606d99635
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:35:49 -0700

    Test: Item adds bids to bid hash

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,10 +1,13 @@
 require 'rspec'
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do
   before (:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
   end
 
   it 'exists' do
@@ -18,6 +21,13 @@ RSpec.describe Item do
 
   it "holds empty hash of bids by default" do
     expect(@item1.bids).to eq({})
+  end
+
+  it "adds bids to bids hash" do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    expect(@item1.bids).to eq({@attendee2 => 20,
+      @attendee2 => 22})
   end
 
 end
