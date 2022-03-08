3f68d880500eb2896972245d07da0c7915e79114
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-08 09:22:38 -0700

    Test: Auction can list items by name

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -20,4 +20,10 @@ describe Item do
     @auction.add_item(@item2)
     expect(@auction.items).to eq([@item1, @item2])
   end
+
+  it "can list items by name" do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
 end
