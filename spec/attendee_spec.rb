5069f31182d9c12af92a315798d96c423dea2060
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:10:21 -0700

    Test: Attendee exists

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/item'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  before (:each) do
+    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+  end
+
+  it 'exists' do
+    expect(@attendee).to be_an_instance_of(Attendee)
+  end
+end
