bfa9aa0eb95c8d7c5ffa03194b89ab999d64fe88
Will Medders <wmedders@gmail.com>
2022-03-08 11:48:42 -0500

    Spec: Item #current_high_bid

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -60,5 +60,16 @@ RSpec.describe Auction do
       @item1.add_bid(@attendee1, 22)
       expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
+
+    it 'Item #current_high_bid' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+    end
   end
 end
