16242a4de258d75e4cc3c1b2562c983b04dc87c2
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-08 10:02:52 -0700

    Feat: #close_bidding uses new isntance @open that dictates whether item is able to be bid on

--- lib/item.rb
+++ lib/item.rb
@@ -7,14 +7,19 @@ class Item
   def initialize(name)
     @name = name
     @bids = {}
+    @open = true
   end
 
   def add_bid(attendee, amount)
-    @bids[attendee] = amount
+    @bids[attendee] = amount if @open == true
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @open = false
   end
 
 end
