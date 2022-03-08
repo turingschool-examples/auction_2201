3ef4b15ea35c984991d115e2a23a084f2923a496
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 09:53:09 -0700

    Test: Auction #unpopular_items and #potential_revenue

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,28 +2,67 @@ require './lib/auction'
 
 RSpec.describe Auction do
 
-  before :each do
-    @auction = Auction.new
-    @item1 = Item.new('Chalkware Piggy Bank')
-    @item2 = Item.new('Bamboo Picture Frame')
+  context "Iteration 1" do
+
+    before :each do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    it "initializes with an empty items attribute" do
+      expect(@auction.items).to eq([])
+    end
+
+    it "adds items through an add_item method" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+
+      expect(@auction.items).to eq([@item1, @item2])
+    end
+
+    it "returns item names through a method item_names" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+
+      expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+
   end
 
-  it "initializes with an empty items attribute" do
-    expect(@auction.items).to eq([])
-  end
+  context "Iteration 2" do
 
-  it "adds items through an add_item method" do
-    @auction.add_item(@item1)
-    @auction.add_item(@item2)
+    before :each do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
 
-    expect(@auction.items).to eq([@item1, @item2])
-  end
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
 
-  it "returns item names through a method item_names" do
-    @auction.add_item(@item1)
-    @auction.add_item(@item2)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+    end
 
-    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    it "returns unpopular_items, items with no bids" do
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
+    end
+
+    xit "returns potential revenue from all highest bids" do
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.potential_revenue).to eq(87)
+    end
+
+
   end
 
 end
