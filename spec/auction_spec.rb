894c768581d2463ecd432fb85bb66054efdde551
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:19:38 -0500

    Test: write test for Auction exist

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,3 +2,10 @@ require 'rspec'
 require './lib/item'
 require './lib/attendee'
 require './lib/auction'
+
+RSpec.describe Auction do
+  it "exists" do
+    auction = Auction.new
+    expect(auction).to be_a(Auction)
+  end
+end
