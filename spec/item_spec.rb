bb1d6a7d2ed6e9f15266152a3cb169bee61d4c1b
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:03:49 -0700

    Test: Item exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  before (:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+  end
+
+  it 'exists' do
+    expect(@item1).to be_an_instance_of(Item)
+  end
+end
