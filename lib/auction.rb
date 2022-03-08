ba8725c252079711df0561def95f68ed6f96fa53
Kim G <xiaole.guo@yahoo.com>
2022-03-08 09:43:23 -0700

    Auction calculates potential revenue with current high bid

--- lib/auction.rb
+++ lib/auction.rb
@@ -15,7 +15,13 @@ attr_reader :items
 
   def unpopular_items
     @items.find_all{|item| item.bids =={}}
+  end
 
+  def potential_revenue
+    valid_high_bids = @items.map{|item| item.current_high_bid}
+    potential_revenue = 0
+    valid_high_bids.map{|bid| potential_revenue += bid if bid != nil }
+    potential_revenue
   end
 
 end
