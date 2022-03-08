de2be246e0e3f76d9f643e9eff99395c9521c99c
Andrew Speth <speth.andrew@yahoo.com>
2022-03-08 09:25:37 -0700

    test: Auction can take bids

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,14 +1,38 @@
 require './lib/auction'
 
 RSpec.describe Auction do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+    @auction = Auction.new
+  end
+
   it "Auction has attributes" do
-    item1 = Item.new('Chalkware Piggy Bank')
-    item2 = Item.new('Bamboo Picture Frame')
-    auction = Auction.new
-    expect(auction.items).to eq([])
-    auction.add_item(item1)
-    auction.add_item(item2)
-    expect(auction.items).to eq([item1, item2])
-    expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    expect(@auction.items).to eq([])
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.items).to eq([@item1, @item2])
+    expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+  end
+
+  it "can take bids" do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    expect(@item1.bids).to eq({})
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
   end
 end
