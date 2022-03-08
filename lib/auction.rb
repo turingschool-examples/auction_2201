079e182645e56245547544368b3bec84485995ce
Susan Boyd <sueboyd922@gmail.com>
2022-03-08 11:19:53 -0500

    Finish Iteration 1

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,6 +1,18 @@
 require './lib/attendee'
 
 class Auction
+  attr_reader :items
 
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def item_names
+    @items.map {|item| item.name}
+  end
 
 end
