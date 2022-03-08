75642e68302ebed3c2929185a1fcb3277bdbd80c
James Harkins <jh.harkins@protonmail.com>
2022-03-08 09:18:12 -0700

    test: create tests for auction class

---
+++ spec/auction_spec.rb
@@ -0,0 +1,29 @@
+require './lib/auction'
+require 'pry'
+
+RSpec.describe Auction do
+
+  before(:each) do
+    @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  context 'Iteration 1' do
+    it 'is initialized without any items' do
+      expect(@auction.items).to eq([])
+    end
+
+    it 'can add items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq([@item1, @item2])
+    end
+
+    it 'can identify the names of each item in its collection' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    end
+  end
+end
