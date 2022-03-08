55ed758d04d3629b4ad5ed38c921207c9634c360
Susan Boyd <sueboyd922@gmail.com>
2022-03-08 11:59:40 -0500

    Feat: add close_bidding method, update add_bid method to not take bid if bidding closed

--- lib/item.rb
+++ lib/item.rb
@@ -4,15 +4,20 @@ class Item
   def initialize(name)
     @name = name
     @bids = {}
+    @bidding_open = true
   end
 
   def add_bid(attendee, bid)
-    @bids[attendee] = bid
+    @bids[attendee] = bid if @bidding_open
   end
 
   def current_high_bid
     return 0 if @bids.values.sort.last.nil?
     @bids.values.sort.last
+  end
+
+  def close_bidding
+    @bidding_open = false
   end
 
 end
