95dbef979a1bfc13d1d8bb61317b6b50af4e503c
Chris Romero <chrisleeromero1@gmail.com>
2022-03-08 09:49:25 -0700

    Feat: add bids method for item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,14 @@
 class Item
 
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] = bid
   end
 
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe Auction do
     @item3 = Item.new('Homemade Chocolate Chip Cookies')
     @item4 = Item.new('2 Days Dogsitting')
     @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
 
@@ -33,6 +36,23 @@ RSpec.describe Auction do
 
   it 'has bids for items' do
     expect(@item1.bids).to eq({})
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    expect(@item1.bids).to eq(
+      {
+        @attendee2 => 20,
+        @attendee1 => 22
+      }
+    )
+    #=> {
+    #     #<Attendee:0x00007fdc071131c8 ...> => 20,
+    #     #<Attendee:0x00007fdc088f0e08 ...> => 22
+    #   }
   end
 
 end
