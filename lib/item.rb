73e31ca0ed67e8267c088fb0bc7b84b0f44fc00c
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 10:52:56 -0700

    Feat: Item #close_bidding

--- lib/item.rb
+++ lib/item.rb
@@ -5,16 +5,21 @@ class Item
   def initialize (name)
     @name = name
     @bids = {}
+    @closed = false
   end
 
   def add_bid (attendee_obj, bid)
-    @bids[attendee_obj] = bid
+    @bids[attendee_obj] = bid unless @closed == true
   end
 
   def current_high_bid
     high_bid = 0
     @bids.each_value {|value| high_bid = value if value > high_bid}
     high_bid
+  end
+
+  def close_bidding
+    @closed = true
   end
 
 end
