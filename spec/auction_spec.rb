464d92cc61c9631f3a79203b2210705f93501d96
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-08 09:51:05 -0700

    Test: Auction returns potential revenue

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -49,6 +49,23 @@ RSpec.describe Auction do
     @item4.add_bid(@attendee3, 50)
 
     expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+
+    @item3.add_bid(@attendee2, 15)
+    expect(@auction.unpopular_items).to eq([@item2, @item5])
+  end
+
+  it 'identifies potential revenue' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+    
+    expect(@auction.potential_revenue).to eq(87)
   end
 
 end
