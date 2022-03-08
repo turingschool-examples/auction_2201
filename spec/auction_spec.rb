ecc9fea5be76518c010b936ffe0e7e55d81fe827
Luke Pascale <lpluke5@gmail.com>
2022-03-08 10:11:18 -0700

    test: Write test for high_bid

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -65,5 +65,12 @@ RSpec.describe Auction do
 
       expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
+
+    it 'can return the current high bid on an item' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+
+      expect(@item1.current_high_bid).to eq(22)
+    end
   end
 end
