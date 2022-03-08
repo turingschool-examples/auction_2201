a9e3a78614cbba261c64cf6f4386e97369b9fb5a
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:06:45 -0500

    Test: name test for Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Item do
     item1 = Item.new('Chalkware Piggy Bank')
     expect(item1).to be_a(Item)
   end
+
+  it "has a name" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1.name).to eq('Chalkware Piggy Bank')
+  end 
 end
