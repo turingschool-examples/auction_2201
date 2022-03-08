e58f1f1042b88a9fa14a54de5c915a34116da59e
Luke Pascale <lpluke5@gmail.com>
2022-03-08 09:46:16 -0700

    test: Setup auction environment with before(:each)

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe Auction do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
-    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     @auction = Auction.new
   end
   describe '#auction' do
@@ -33,6 +38,28 @@ RSpec.describe Auction do
       @auction.add_item(@item2)
 
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+
+  describe "#bidding" do
+    before(:each) do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+    end
+    it 'can hold the items' do
+      # @auction.add_item(@item1)
+      # @auction.add_item(@item2)
+      # @auction.add_item(@item3)
+      # @auction.add_item(@item4)
+      # @auction.add_item(@item5)
+      expect(@auction.items).to eq([@item1, @item2, @item3, @item4, @item5])
+    end
+
+    xit 'can read bids' do
+
     end
   end
 end
