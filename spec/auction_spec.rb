be005cf36109ef299bda18b67f0e0ae866a9462f
Alex Psitos <apsitos08@gmail.com>
2022-03-08 11:26:05 -0500

    Test: write test for read item names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -21,5 +21,14 @@ RSpec.describe Auction do
     auction.add_item(item1)
     auction.add_item(item2)
     expect(auction.items).to eq([item1, item2])
-  end 
+  end
+
+  it "can read item names" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
 end
