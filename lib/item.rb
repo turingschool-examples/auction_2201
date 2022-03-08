1054718c55ae090bb89a6d796a5dfd1f8073930c
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 09:42:16 -0700

    Feat: Item #add_bid

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,14 @@
 class Item
 
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize (name)
     @name = name
     @bids = {}
   end
-  
+
+  def add_bid (attendee_obj, bid)
+    @bids[attendee_obj] = bid
+  end
+
 end
