1f79cdf12ff00697b6412421e6a8772f0d3c3c10
Joseph Downs <jos.w.downs@gmail.com>
2022-03-08 10:42:04 -0700

    Add feat: close bidding

--- lib/item.rb
+++ lib/item.rb
@@ -4,15 +4,21 @@ class Item
   def initialize(name)
     @name = name
     @bids = {}
+    @bidding = "open"
   end
 
   def add_bid(attendee, bid)
-    @bids[attendee] = bid
+    if @bidding == "open"
+      @bids[attendee] = bid
+    end
   end
 
   def current_high_bid
-    bids.values.max
+    @bids.values.max
   end
 
+  def close_bidding
+    @bidding = "closed"
+  end
 
 end
