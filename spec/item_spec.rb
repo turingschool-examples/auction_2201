01a828501a8e1bbbfb5daa79e6ba2074e059ffc5
Will Medders <wmedders@gmail.com>
2022-03-08 11:10:51 -0500

    Spec: item has attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Item do
   it 'exists' do
     expect(@item1).to be_a(Item)
   end
+
+  it 'has attributes' do
+    expect(@item1.name).to eq('Chalkware Piggy Bank')
+    expect(@item1.bids).to eq({})
+  end
+
 end
