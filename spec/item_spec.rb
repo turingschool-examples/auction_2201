3d5ed95133a0257aca71cb0538aaa2ceb00b2ba4
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-08 10:06:33 -0700

    Test: Items have no bids at start

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -21,6 +21,32 @@ RSpec.describe Item do
     it "has a name" do
       expect(@item1.name).to eq("Chalkware Piggy Bank")
     end
-
   end
+
+  context "Iteration 2" do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+      @auction = Auction.new
+
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+    end
+
+    it "has no bids by default" do
+      expect(@item1.bids).to eq({})
+    end
+  end
+
 end
