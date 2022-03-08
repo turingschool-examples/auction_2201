2d686a643c18902377eebc4c099e1aa35b9a9897
Sherman Au <shermau13@gmail.com>
2022-03-08 09:06:02 -0700

    intial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require './lib/item'
+
+RSpec.describe Item do
+  context "Iteration 1" do
+    before(:each) do
+
+    end
+
+    xit 'exists ' do
+      expect().to be_an_instance_of()
+    end
+
+  end
+  # context "Iteration 2" do
+  #   before(:each) do
+
+  #   end
+
+  #   xit ' ' do
+  #     expect().to eq()
+  #   end
+
+  #   xit ' ' do
+  #     expect().to eq()
+  #   end
+
+  #   xit ' ' do
+  #     expect().to eq()
+  #   end
+  # end
+
+end
