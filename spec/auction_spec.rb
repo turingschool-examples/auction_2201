932c508657b0af2b9ff29c9cee2259524aaaeb0d
Rin Fabian <pfabianschool@gmail.com>
2022-03-08 10:42:31 -0600

    test;#unpopular_items for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -31,4 +31,18 @@ RSpec.describe Auction do
     expect(@auction.item_names).to eq(["Chalkware Piggy Bank","Bamboo Picture Frame"])
   end
 
+  it 'can decide the unpopular items' do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    expect(@auction.unpopular_items).to eq([@item2,@item3,@item5])
+  end
+
+
 end
