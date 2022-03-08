8f14081c78848d2c3a88c6ca39dfdd0e8c54879c
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-08 08:24:00 -0800

    Test: add inital auction test with accompanying initialize

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,1 +1,12 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
 
+  RSpec.describe Auction do
+    it 'exists and has an empty array' do
+      auction = Auction.new
+      expect(auction).to be_an_instance_of(Auction)
+      expect(auction.items).to be_a(Array)
+      expect(auction.items).to eq([])
+    end
+  end
