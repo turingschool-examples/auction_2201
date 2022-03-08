332c606be9985f085c1e0d1933553426322c624c
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 09:31:43 -0700

    Feat: item #add_bid passing test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -44,7 +44,7 @@ describe Item do
       @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
 
-      expect(@item1.bids).to eq({@attendee2 => 20, @attendee3 => 22})
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
   end
 end
