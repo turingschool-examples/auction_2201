607fe6071b1952b2a52f2a0f8df423d874766bc1
“Brad <hutch13531@gmail.com>
2022-03-08 11:31:10 -0500

    added test to check for items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe do
     auction = Auction.new
     expect(auction).to be_an_instance_of(Auction)
   end
+
+  it "has items" do
+    auction = Auction.new
+    expect(auction.items).to eq([])
+  end
 end
