d27a124928b3a6f684de04718f7c50c4dfd5b132
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-08 09:09:50 -0700

    Initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/item"
+
+RSpec.describe Item do
+  it "exists/ has readable attributes" do
+    item1 = Item.new("Chalkware Piggy Bank")
+    expect(item1).to be_an_instance_of(Item)
+    expect(item1.name).to eq("Chalkware Piggy Bank")
+    expect(item1.bids).to eq({})
+  end
+end
