dd2c008d0a9321c0e0a21e62406a29fe7dd46128
Rin Fabian <pfabianschool@gmail.com>
2022-03-08 10:58:15 -0600

    test;#close_bidding for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Item do
     @item2 = Item.new('Bamboo Picture Frame')
     @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
     @attendee2 = Attendee.new({name: 'Charlie', budget: '$100'})
+    @attendee3 = Attendee.new({name: 'Jazz', budget: '$80'})
   end
 
   it 'has a name' do
@@ -28,6 +29,14 @@ RSpec.describe Item do
     @item1.add_bid(@attendee1, 20)
     @item1.add_bid(@attendee2, 24)
     expect(@item1.current_high_bid).to eq(24)
+  end
+
+  it 'can close bidding' do
+    @item1.add_bid(@attendee1, 22)
+    @item1.add_bid(@attendee2, 20)
+    @item1.close_bidding
+    @item1.add_bid(@attendee3, 70)
+    expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})
   end
 
 end
