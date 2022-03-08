b793c7628b989e997fe6ce77fc1b84f7b36b4c2f
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-08 09:13:56 -0700

    Test: Attendee exists and has readable attributes

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/item"
+
+describe Item do
+  before(:each) do
+    @item1 = Item.new('Chalkware Pggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+  end
+
+  it "exists and has readable attributes" do
+    expect(@attendee).to be_a(Attendee)
+    expect(@attendee.name).to eq("Megan")
+    expect(@attendee.budget).to eq(50)
+  end
+end
