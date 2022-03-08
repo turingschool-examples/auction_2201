15f95736cb42cb444ce500ddd8ae06e4944ce09b
reuben <creubend@gmail.com>
2022-03-08 09:27:56 -0700

    test: Add item test created

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -7,11 +7,18 @@ RSpec.describe Attendee do
   
   before :each do
     @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end 
   
   it "exists" do
     expect(@auction).to be_a(Auction)
     expect(@auction.items).to eq([])
+  end
+  it "adds an item" do
+    auction.add_item(@item1)
+    auction.add_item(@item2)
+    expect(auction.items).to eq ([@item1, @item2])
   end
 
 
