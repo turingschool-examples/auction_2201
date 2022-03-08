f9f0a5a3ff4bd9f569381a0216a85fb755b72a2d
Will Medders <wmedders@gmail.com>
2022-03-08 11:08:08 -0500

    Feat: item exists

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,11 +4,11 @@ require 'rspec'
 
 RSpec.describe Item do
   before :each do
-    item1 = Item.new('Chalkware Piggy Bank')
-    item2 = Item.new('Bamboo Picture Frame')
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
   it 'exists' do
-    expect(item1).to be_a(Item)
+    expect(@item1).to be_a(Item)
   end
 end
