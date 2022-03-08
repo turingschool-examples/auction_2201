9fbbecbdad23d74da2e6234a26eb47e1eeabbf9f
Andrew Speth <speth.andrew@yahoo.com>
2022-03-08 09:59:58 -0700

    test: can stop bidding

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -106,5 +106,12 @@ RSpec.describe Auction do
     it "can return bidder names" do
       expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
     end
+
+    it "can stop bidding" do
+      expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})
+      @item1.close_bidding
+      @item1.add_bid(@attendee3, 70)
+      expect(@item1.bids).to eq({@attendee1 => 22, @attendee2 => 20})
+    end
   end
 end
