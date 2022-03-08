22cbb5ab697336fa4f6bc4de3085f8faaa574325
Joseph Senecal <jasenecal2@gmail.com>
2022-03-08 09:59:47 -0700

    Fix: add items to auction in before block

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -44,6 +44,12 @@ RSpec.describe Auction do
       @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
       @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
 
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
       @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
       @item4.add_bid(@attendee3, 50)
