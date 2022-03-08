ed036c26303dc0048c8b1c4be77306f260f71977
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-08 09:55:17 -0700

    Test: add test for potential_revenue method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -102,6 +102,14 @@ RSpec.describe Item do
       expect(@auction.unpopular_items).to eq([@item2, @item5])
     end
 
+    it 'can return the potential_revenue for all item bids' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.potential_revenue).to eq(87)
+    end
+
 
 
 
